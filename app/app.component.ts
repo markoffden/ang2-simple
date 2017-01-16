d7b1cd511cec8c05d9286bbce9157f11b7c124e1
Denys Markov <denys.markov@n3wnormal.com>
2017-01-16 16:44:37 +0200

    components setup

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,7 +1,8 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app',
-  template: `<h1>Hello {{name}}</h1>`,
+    moduleId: module.id,
+    selector: 'app',
+    templateUrl: 'app.component.html'
 })
-export class AppComponent  { name = 'Denys'; }
+export class AppComponent  { name = 'Denys'}
