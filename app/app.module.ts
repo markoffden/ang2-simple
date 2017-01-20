655519b30343a3ce6033b1807c1a2e2386feb7ce
Denys Markov <denys.markov@n3wnormal.com>
2017-01-20 17:53:35 +0200

    final commit, setting up templates and search functionality

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpModule } from '@angular/http';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent }  from './app.component';
 import { NavbarComponent }  from './components/navbar/navbar.component';
@@ -12,7 +13,7 @@ import { GithubSearchComponent } from './components/routes/github-search/github-
 import { ProfileComponent } from './components/profile/profile.component';
 
 @NgModule({
-  imports:      [ BrowserModule, routing, HttpModule ],
+  imports:      [ BrowserModule, routing, HttpModule, FormsModule ],
   declarations: [
       AppComponent,
       NavbarComponent,
--- app/components/profile/profile.component.ts
+++ app/components/profile/profile.component.ts
@@ -8,9 +8,23 @@ import { GithubService } from '../../services/github.service';
 })
 
 export class ProfileComponent {
+    user: any;
+    repos: any[];
+    username: string;
+
     constructor(private _githubService: GithubService) {
+        this.user = false;
+    }
+
+    searchUser() {
+        this._githubService.updateUsername(this.username);
+
         this._githubService.getUser().subscribe(user => {
-            console.log(user);
+            this.user = user;
+        });
+
+        this._githubService.getRepos().subscribe(repos => {
+            this.repos = repos;
         });
     }
 }
